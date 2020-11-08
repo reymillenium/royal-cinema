747bb911269f38551ddd66d727b1fb63b8a60933
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-08 02:16:08 -0500

    Added the index method on the pages_controller. Added the gross of the html code from the template and into the app;ication layout. Added the root rout and the get route or the pages/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # root :to => redirect("/pages")
+  root :to => redirect("pages")
+
+  # The extra_pages view (extra)
+  get 'pages', to: 'pages#index'
 end
