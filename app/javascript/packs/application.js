3796960b006d205808a04ece4701e4970cb13e35
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-08 01:50:19 -0500

    Added configuration for fontawesome at 'app/assets/stylesheets/application.scss' and 'app/javascript/packs/application.js'

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 
-
 // Default:
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -19,6 +18,9 @@ document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
 });
+
+// FontAwesome scripting:
+import "@fortawesome/fontawesome-free/js/all";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
