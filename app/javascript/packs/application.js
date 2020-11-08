6ae081ed6ad09951f853683ff91ee892b79a3eb4
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-08 01:46:14 -0500

    Copied all the images, css and .js files into the app, from its template. Added config at 'app/javascript/packs/application.js:16' in order to enable the tooltips & popovers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,12 @@ require("channels")
 // Boostrap 4:
 import 'bootstrap'
 
+// Enables the tooltips & popovers:
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
