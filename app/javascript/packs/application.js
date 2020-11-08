0a3e1ac2a00690d63253e43fb4498ac0d4be930c
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-08 18:38:43 -0500

    Fixed issue with skillbars, by using the original jquery.mobile.custom.min (version 1.4.2) file instead of the newer jquery.mobile-1.5.0-rc1.min

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,35 +28,11 @@ import './responsiveslides'
 // import './SmoothScroll.min'
 import './SmoothScroll'
 
-// Custom js files:
-// require("easing")
-// require("move-top")
-// require("responsiveslides")
-// require("SmoothScroll.min")
-
 import './jquery.cm-overlay'
 import './jquery.cntl'
-import './jquery.mobile-1.5.0-rc1.min'
+// import './jquery.mobile-1.5.0-rc1.min'
 import './jquery.tools.min'
 import './skill.bars.jquery'
-// import './jquery.mobile.custom.min'
+import './jquery.mobile.custom.min'
 
 
-
-
-// require("jquery.cm-overlay")
-// require("jquery.cntl")
-// require("jquery.mobile.custom.min")
-// require("jquery.tools.min")
-// require("skill.bars.jquery")
-
-
-
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
