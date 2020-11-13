8d1ebf318ff752874db68526fc8106310bfa2741
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-13 12:39:03 -0500

    Now it finally sends the damn emails

--- Gemfile
+++ Gemfile
@@ -37,8 +37,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # gem 'dotenv-rails'
-  gem "figaro"
 end
+  gem 'figaro'
 
 gem 'lightbox2-rails'
 gem 'listen', '~> 3.2'
--- app/services/contact_service.rb
+++ app/services/contact_service.rb
@@ -24,11 +24,12 @@ class ContactService
     content = Content.new(type: 'text/plain', value: contact.message)
     mail = Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    sg = SendGrid::API.new(api_key: Figaro.env.sendgrid_api_key)
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    # puts response.status_code
-    # puts response.body
-    # puts response.headers
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 
   # Cleans parameters not applicable to note attributes
