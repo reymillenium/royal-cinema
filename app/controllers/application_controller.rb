e654a71d64a414f13ec69eb1e20f31a273f2f364
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-15 21:39:02 -0500

    Added the scss file login, to be used only with the login layout (devise). Added the login layout for devise. Added animate css file to deal with the animations. Modified most of the devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+  layout :layout_by_resource
+
+  protect_from_forgery
+  # before_action :authenticate_user!
+
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
+
+  # def after_sign_in_path_for(resource_or_scope)
+  #   contacts_path
+  # end
+  #
+  # def after_sign_out_path_for(resource_or_scope)
+  #   new_session_path(resource_or_scope)
+  # end
+
 end
