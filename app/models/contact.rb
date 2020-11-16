ee105ca1c462b4ac1124e3856203785ed0777ab7
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-15 21:49:39 -0500

    Added the fields first_name and last_name to the User model (users table) using a migration file. Added the has_many relation contacts to the User model and the belongs_to relation user to the Contact model.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,25 @@
 class Contact < ApplicationRecord
+
+  # Trix ActionText
+  # has_rich_text :body
+
+  # Relations:
+  belongs_to :user, class_name: 'User', inverse_of: :contacts
+  # has_one :action_text_rich_text,
+  #         class_name: 'ActionText::RichText',
+  #         as: :record
+
+  # Validations:
+  validates :title, presence: true
+  validates :title, length: { maximum: 30 }
+  validates :body, length: {
+    maximum: 1000,
+    tokenizer: lambda { |str| str.scan(/./) }
+  }
+
+  # Scopes:
+  scope :visible_by, ->(user) {
+    where(user_id: user)
+  }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Relations
+  has_many :contacts, inverse_of: :user, foreign_key: :user_id, dependent: :destroy
+
 end
