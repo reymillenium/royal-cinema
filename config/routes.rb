45c99aded93fc47a796c374bd2ff7fc09aff1f91
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-15 20:39:02 -0500

    Generated the user model with devise. Ran the migrations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  # devise_for :users, controllers: {
+  #   # confirmations: 'users/confirmations'
+  #   confirmations: 'users/confirmations',
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions'
+  # }
 
   resources :contacts
 
