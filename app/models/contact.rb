a82fa2aa422553b7e45946e28f78f8ec954f017f
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-18 10:53:02 -0500

    Added profanity validation on the contact model, for the fields: subjects and message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,17 @@
 class Contact < ApplicationRecord
 
+  require 'obscenity/active_model'
+
   # Validations:
   validates :name, presence: true
+
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   validates :subject, presence: true
+  validates :subject, obscenity: true
+
   validates :message, presence: true
+  validates :message, obscenity: true
 
 end
