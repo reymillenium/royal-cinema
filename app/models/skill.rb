b035d651abaa0d6c4678ce3cb4e74080a2db9407
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-24 20:14:03 -0500

    Modified the app/models/skill.rb by adding uniqueness validation for the name field

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,6 +4,7 @@ class Skill < ApplicationRecord
 
   # Validations:
   validates :name, presence: true
+  validates :name, uniqueness: true
   validates :color, presence: true
   validates :percentage, presence: true
   validates :percentage, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
