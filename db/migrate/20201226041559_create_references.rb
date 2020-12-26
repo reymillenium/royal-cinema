8fb7075be4a99291946a639dcc7f9862006f5c32
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-25 23:35:09 -0500

    Generated model Reference with its migration file and other files as well

---
+++ db/migrate/20201226041559_create_references.rb
@@ -0,0 +1,12 @@
+class CreateReferences < ActiveRecord::Migration[6.0]
+  def change
+    create_table :references do |t|
+      t.string :full_name
+      t.string :current_job_title
+      t.string :linkedin_profile
+      t.string :mobile_number
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_090849) do
+ActiveRecord::Schema.define(version: 2020_12_26_041559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_12_24_090849) do
     t.string "email"
     t.string "subject"
     t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "references", force: :cascade do |t|
+    t.string "full_name"
+    t.string "current_job_title"
+    t.string "linkedin_profile"
+    t.string "mobile_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
