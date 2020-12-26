35ece329d43969b3002d48787fb372fa00405e32
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-26 01:58:59 -0500

    Modified app/views/layouts/admin_layout.html.erb by adding the references link. Modified the views by adding the ransack search criteria. Mod the app/models/reference.rb by adding the has_one property ActionText::RichText

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -3,4 +3,9 @@ class Reference < ApplicationRecord
   # Trix ActionText
   has_rich_text :letter
 
+  #  Relations
+  has_one :action_text_rich_text,
+          class_name: 'ActionText::RichText',
+          as: :record
+
 end
