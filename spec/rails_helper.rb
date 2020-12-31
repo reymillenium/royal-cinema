2a59ffd1fcc90529ee8fc700a63f6f5c43749621
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-30 19:40:27 -0500

    Modified spec/rails_helper.rb by adding config to use the models with shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Allows to test the relations between the models with shoulda-matchers:
+  Shoulda::Matchers.configure do |shoulda|
+    shoulda.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
