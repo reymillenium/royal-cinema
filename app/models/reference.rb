254046c5236f1bb0ae4d5c32c25207b5fa60b4ab
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-06 17:42:18 -0500

    Modified app/models/reference.rb by adding validation

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -8,4 +8,10 @@ class Reference < ApplicationRecord
           class_name: 'ActionText::RichText',
           as: :record
 
+  # Validations:
+  validates :full_name, presence: true
+  validates :current_job_title, presence: true
+  validates :linkedin_profile, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, allow_nil: true }
+
 end
