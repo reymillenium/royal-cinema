cf8424f0d35a8c247822857ac770490380f5eb89
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-06 18:49:01 -0500

    Modified app/controllers/pages_controller.rb by retrieving also all the sample objects on the index method. Added the partial app/views/pages/_samples_dynamic.html.erb which renders dynamically the sample objects. Modified app/views/pages/index.html.erb by rendering the partial 'samples_dynamic' insteasd of 'samples'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def index
     @skills = Skill.all.order(percentage: :desc)
     @references = Reference.all.order(pertinent_date: :desc)
+    @samples = Sample.with_attached_image.all.order(title: :desc)
   end
 
 end
