d9b27dbcace6d9372a7b193c869b4157ed5bbdd9
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-21 00:13:58 -0500

    Generated the Auditorium model, as well as its factory, migration file and rspec file

---
+++ app/models/auditorium.rb
@@ -0,0 +1,6 @@
+class Auditorium < ApplicationRecord
+
+  # Validations:
+  validates :name, presence: true
+
+end
---
+++ db/migrate/20210121045650_create_auditorium.rb
@@ -0,0 +1,9 @@
+class CreateAuditorium < ActiveRecord::Migration[6.0]
+  def change
+    create_table :auditorium do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
